4adfb3a14067302c167431184bda768227b305c0
devkarim <devkarim@hotmail.com>
2022-09-24 23:39:27 +0200

    remove update location button

--- src/pages/main/home.tsx
+++ src/pages/main/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SafeArea from '@src/layout/SafeArea';
 import Content from '@src/layout/Content';
 import BasicInfo from '@src/features/misc/components/BasicInfo';
@@ -7,34 +7,8 @@ import NextPrayerCard from '@src/features/prayers/components/NextPrayerCard';
 import tw from '../../lib/tailwind';
 import Txt from '../../components/ui/Txt';
 import Space from '../../layout/Space';
-import Button from '../../components/ui/Button';
-import Palette from '../../config/palette';
-import * as Location from 'expo-location';
-import { debug } from '../../helpers/utils';
-import { saveLocation } from '../../state/reducers/local';
-import { useAppDispatch } from '../../state/hooks';
 
 const HomeScreen = () => {
-  const dispatch = useAppDispatch();
-  const [status, setStatus] = useState('Hi');
-
-  // this function is for debugging on android device
-  const updateLoc = async () => {
-    setStatus('updating loc...');
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    setStatus(
-      `got status ${JSON.stringify(
-        status
-      )}. if granted, please wait until it updates...`
-    );
-    if (status !== 'granted') return;
-
-    let location = await Location.getCurrentPositionAsync({});
-    setStatus(`got loc ${JSON.stringify(location)}`);
-    debug('Set new location', location);
-    await dispatch(saveLocation(location));
-  };
-
   return (
     <SafeArea>
       <Content>
@@ -43,14 +17,6 @@ const HomeScreen = () => {
         <NextPrayerCard />
         <HorizontalLine style={tw`w-full my-8`} />
         <Txt style={tw`font-extrabold text-5xl`}>Up Next</Txt>
-        <Space />
-        <Button
-          title="Update Location"
-          bgColor={Palette.PRIMARY}
-          onPress={updateLoc}
-        />
-        <Space />
-        <Txt>{status}</Txt>
       </Content>
     </SafeArea>
   );
