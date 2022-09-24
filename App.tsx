e530780b63140ca0cea91bef438543ad61d06bd2
devkarim <devkarim@hotmail.com>
2022-09-24 23:43:35 +0200

    fix force ltr not working

--- App.tsx
+++ App.tsx
@@ -1,4 +1,13 @@
 import Root from './src';
+import { I18nManager } from 'react-native';
+
+/*
+  Forces the application to be Left-To-Right (LTR).
+
+  TODO: Remove when more languages are supported.
+*/
+I18nManager.forceRTL(false);
+I18nManager.allowRTL(false);
 
 export default function App() {
   return <Root />;
